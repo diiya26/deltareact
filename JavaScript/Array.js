67f247e7e2bd060b19c6ab1550d6a3c42494d531
Diya Prajapati <prajapatidia4@gmail.com>
2026-07-24 22:02:15 +0530

    Updated DeltaReact project

--- JavaScript/Array.js
+++ JavaScript/Array.js
@@ -1,2 +1,17 @@
-constoriginal_array= [23,45,78,21,98,12,45,67];
+const original_array= [23,45,78,21,98,12,45,67];
 console.log(original_array);
+
+const doubleArray=original_array.map(num=>num*2);
+console.log(doubleArray);
+
+const sum=original_array.reduce((total, num) =>total+num, 0);
+console.log("Sum of All Elements:", sum);
+
+const studentMarks= [75, 92, 51, 64, 76, 68, 59, 85, 65];
+console.log("Student Marks:", studentMarks);
+
+const highMarks=studentMarks.filter(mark=>mark>75);
+console.log("Marks Greater Than 75:", highMarks);
+
+const sortedNumbers= [...studentMarks].sort((a, b) =>a-b);
+console.log("Sorted Array (Ascending):", sortedNumbers);
