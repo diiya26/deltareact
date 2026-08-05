e4ab77a24b2c5baeffe2b8bafab597cabce994b7
Diya Prajapati <prajapatidia4@gmail.com>
2026-08-05 21:14:00 +0530

    updated reactrouter project

--- delta-react-app/src/App.jsx
+++ delta-react-app/src/App.jsx
@@ -1,19 +1,8 @@
-import { createContext } from "react";
-import reactLogo from'./assets/react.svg'
-import viteLogo from'./assets/vite.svg'
-import heroImg from'./assets/hero.png'
-import'./App.css'
-import Home from'./components/Home'
-
-exportconstUserContext=createContext();
-functionApp() {
+import MsgBox from "./assets/Component/MsgBox";
+function App() {
     return (
     <>
-    <div>
-     <UserContext.Provider value="Raj Mehta">
-      <Home/>
-     </UserContext.Provider>
-    </div>
+    <MsgBox username/>
    </>
   )
 }
---
+++ delta-react-app/src/assets/Component/MsgBox.jsx
@@ -0,0 +1,7 @@
+export default function MsgBox ( {userName , textcolor } ) {
+  //  let styles = {color:textcolor};
+    return <h1 style={{ color:textcolor}}>Hello, {username}</h1>;
+
+    
+} 
+export default MsgBox;
