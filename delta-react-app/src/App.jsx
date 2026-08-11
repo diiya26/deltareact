81ab97b2cb183adda1c9ebfdfbfb1d26bcd14895
Diya Prajapati <prajapatidia4@gmail.com>
2026-08-11 22:01:18 +0530

    updated deltareactapp project

--- delta-react-app/src/App.jsx
+++ delta-react-app/src/App.jsx
@@ -1,10 +1,15 @@
 //import Profile from "./assets/Component/Profile";
-import Form from "./assets/Component/Form";
+//import Form from "./assets/Component/Form";
+import TaskApp from "./assets/Component/TaskApp";
+
+import AddTask from './assets/Component/AddTask';
+import TaskList from './TaskList.js';
+
 
 function App() {
     return (
     <>
-    <Form/>
+    <TaskApp/>
    </>
   )
 }
